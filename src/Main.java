6367c441bb1db73ce735f3f1f137479d656dd102
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 11:55:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en klass Point som har två instansvaraibler, x och y
+        // Skapa en klass Shape som har en Point, och en abstrakt metod area
+        //
+        // Skapa en klass Circle som ärver från Shape, och dessutom har en radie
+        // Skapa en klass Rectangle som ärver från Shape, och dessutom har en bredd och höjd
+        //
+        // Skapa några objekt av dessa två klasser och lägg dem i en array
+        // Skriv ut alla objekt i arrayens area
+
+
+    }
+}
